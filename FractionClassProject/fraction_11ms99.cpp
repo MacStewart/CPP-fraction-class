2c8837e40bc27a272a0043a8280004757165b30e
90ne1 <mac.stewart@live.ca>
2015-02-19 17:04:49 -0500

    Private functions and accessors

--- FractionClassProject/fraction_11ms99.cpp
+++ FractionClassProject/fraction_11ms99.cpp
@@ -1,5 +1,57 @@
-/* 
+/*
 Mac Stewart - 11ms99 - 10043669
 Compiled in Visual Studio 2013 on Windows 8.1
 */
+
+using namespace std;
+
+#include "fraction_11ms99.h"
+
+FractionException::FractionException(const string& msg) : message(msg) {
+
+}
+
+string& FractionException::what() {
+	return message;
+}
+
+Fraction::Fraction() {
+	numerator = 0;
+	denominator = 1;
+}
+
+Fraction::Fraction(int num) {
+	numerator = num;
+	denominator = 1;
+}
+
+Fraction::Fraction(int num, int denom) {
+	int GCD = getGCD(num, denom);
+	if (GCD != 1) {
+		num /= GCD;
+		denom /= GCD;
+	}
+	numerator = num;
+	denominator = denom;
+}
+
+int Fraction::getNumerator() const {
+	return numerator;
+}
+
+int Fraction::getDenominator() const {
+	return denominator;
+}
+
+int Fraction::getGCD(int first, int second) const {
+	if (second <= first && first%second == 0) {
+		return second;
+	}
+	else if (second > first) {
+		return getGCD(second, first);
+	}
+	else {
+		return getGCD(second, first%second);
+	}
+}
 
--- FractionClassProject/fraction_11ms99.h
+++ FractionClassProject/fraction_11ms99.h
@@ -1,14 +1,38 @@
+#pragma once
+
+#include <string>
+#include <iostream>
 
 using namespace std;
 
 class FractionException {
 public:
-	FileException(const string&);
+	FractionException(const string&);
 	string& what();
 private:
 	string message;
 };
 
 class Fraction {
+public:
+	Fraction();
+	Fraction(int);
+	Fraction(int, int);
+	int getNumerator() const;
+	int getDenominator() const;
 
+	Fraction& operator+=(const Fraction& rhs);
+	Fraction& operator++();		//Pre Increment
+	Fraction operator++(int);	//Post Incrememnt
+
+private:
+	int numerator;
+	int denominator;
+	int getGCD(int first, int second) const;
 };
+
+Fraction operator+(const Fraction& lhs, const Fraction& rhs);
+Fraction operator-(const Fraction& lhs, const Fraction& rhs);
+Fraction operator*(const Fraction& lhs, const Fraction& rhs);
+Fraction operator/(const Fraction& lhs, const Fraction& rhs);
+ostream& operator<<(ostream& out, const Fraction& fraction);
